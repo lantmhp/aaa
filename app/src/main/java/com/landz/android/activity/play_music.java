d7c839e2131c90b67bf4f48b50953eb0bf69a515
lantmhp <lantmhp2002@gmail.com>
2023-11-26 09:30:26 +0700

    playmusic activity

--- app/src/main/java/com/landz/android/activity/play_music.java
+++ app/src/main/java/com/landz/android/activity/play_music.java
@@ -2,119 +2,256 @@ package com.landz.android.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.AudioManager;
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.MediaController;
 import android.widget.SeekBar;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.landz.android.R;
+import com.landz.android.database.Database;
+import com.landz.android.databinding.ActivityPlayMusicBinding;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import Model.Song;
 
 public class play_music extends AppCompatActivity {
 
-    ImageView play, prev, next, imageView;
-    TextView songTile;
-    SeekBar mSeekBarTime, mSeekBarVol;
-    static MediaPlayer mediaPlayer;
-    private Runnable runnable;
-    private MediaController mediaController;
-    private AudioManager manager;
-    int currentIndex = 0;
+    ActivityPlayMusicBinding binding;
+    MediaPlayer mediaPlayer;
+    private ArrayList<Song> songArrayList=new ArrayList<>();
+    Song song;
+    private Handler handler = new Handler();
+    Database db = new Database(this);
 
+    String path;
+    private int currentIndex =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play_music);
-        play = findViewById(R.id.play);
-        prev = findViewById(R.id.prev);
-        next = findViewById(R.id.next);
-        mSeekBarTime = findViewById(R.id.seekbarTime);
-        mSeekBarVol = findViewById(R.id.seekbarVol);
+        binding = ActivityPlayMusicBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        ArrayList<Integer> songs = new ArrayList<>();
-//        songs.add(0,R.raw.sontung1);
-//        songs.add(1,R.raw.coChacYeuLaDay);
-//        songs.add(2,R.raw.fashionTanGai);
-//        songs.add(3,R.raw.hayTraoChoAnh);
-//        songs.add(4,R.raw.luonYeuDoi);
-//        songs.add(5,R.raw.noiNayCoAnh);
+        String title = getIntent().getStringExtra("title");
+        byte[] byteArray = getIntent().getByteArrayExtra("image");
+        Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        binding.imgPlay.setImageBitmap(bmp);
+        binding.titleSong.setText(title);
+        mediaPlayer = new MediaPlayer();
+        binding.seekbarTime.setMax(100);
+        prepareMediaPlayer();
+        updateSeekbar();
 
-        mediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
-
-        play.setOnClickListener(new View.OnClickListener() {
+        binding.play.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying())
+                {
+                    handler.removeCallbacks(updater);
                     mediaPlayer.pause();
-                } else {
+                    binding.play.setImageResource(R.drawable.ic_pause_24);
+                }else {
                     mediaPlayer.start();
-                    play.setImageResource(R.drawable.ic_pause_24);
+                    binding.play.setImageResource(R.drawable.ic_play);
+                    updateSeekbar();
                 }
             }
+
         });
-        next.setOnClickListener(new View.OnClickListener() {
+        songArrayList = db.getAllSong();
+        int idsong = getIntent().getIntExtra("id", 0);
+        currentIndex = idsong;
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
-            public void onClick(View view) {
-                if (mediaPlayer != null) {
-                    play.setImageResource(R.drawable.ic_pause_24);
+            public void onCompletion(MediaPlayer mp) {
+                binding.next.performClick();
+            }
+        });
+//        int audiosesssionId = mediaPlayer.getAudioSessionId();
+//        if(audiosesssionId != -1){
+//            vi
+//        }
+        binding.next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.play.setImageResource(R.drawable.ic_play);
+                if (currentIndex < songArrayList.size())
+                {
+                    currentIndex ++;
                 }
-                if (currentIndex < songs.size() - 1) {
-                    currentIndex++;
-                } else currentIndex = 0;
-                if (mediaPlayer.isPlaying()) {
+                else {
+                    currentIndex = 1;
+                }
+                if (mediaPlayer.isPlaying()){
                     mediaPlayer.stop();
+                    mediaPlayer.release();
                 }
-                mediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
-                mediaPlayer.start();
+                mediaPlayer = new MediaPlayer();
+                song = db.getsongbyId(currentIndex);
+                binding.titleSong.setText(song.getSong_Title());
+                Bitmap imageContent = BitmapFactory.decodeByteArray(song.getSong_Image(), 0, song.getSong_Image().length);
+                binding.imgPlay.setImageBitmap(imageContent);
+                path = song.getPath();
+                try {
+
+                    mediaPlayer.setDataSource(path);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    binding.end.setText(milliSecondsToTimer(mediaPlayer.getDuration()));
+                    updateSeekbar();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
             }
         });
-        prev.setOnClickListener(new View.OnClickListener() {
+        binding.prev.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (mediaPlayer != null) {
-                    play.setImageResource(R.drawable.ic_pause_24);
-                }
-                if (currentIndex > 0) {
+            public void onClick(View v) {
+                binding.play.setImageResource(R.drawable.ic_play);
+                if (currentIndex >1)
+                {
                     currentIndex--;
-                } else currentIndex = songs.size() - 1;
-                if (mediaPlayer.isPlaying()) {
-                    mediaPlayer.stop();
                 }
-                mediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
-                mediaPlayer.start();
+                else {
+                    currentIndex = songArrayList.size();
+                }
+                song = db.getsongbyId(currentIndex);
+                binding.titleSong.setText(song.getSong_Title());
+                Bitmap imageContent = BitmapFactory.decodeByteArray(song.getSong_Image(), 0, song.getSong_Image().length);
+                binding.imgPlay.setImageBitmap(imageContent);
+                path = song.getPath();
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                }
+                mediaPlayer = new MediaPlayer();
+                try {
+
+                    mediaPlayer.setDataSource(path);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    binding.end.setText(milliSecondsToTimer(mediaPlayer.getDuration()));
+                    updateSeekbar();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
             }
         });
+        binding.forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.seekTo(mediaPlayer.getCurrentPosition()+10000);
+                    updateSeekbar();
+                }
+
+            }
+        });
+        binding.rewind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.seekTo(mediaPlayer.getCurrentPosition()-10000);
+                    updateSeekbar();
+                }
+
+            }
+        });
+        binding.seekbarTime.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent motionEvent) {
+                SeekBar seekBar = (SeekBar) v;
+                int playPosition = (mediaPlayer.getDuration()/100)*seekBar.getProgress();
+                mediaPlayer.seekTo(playPosition);
+                binding.start.setText(milliSecondsToTimer(mediaPlayer.getCurrentPosition()));
+                return false;
+            }
+        });
+        mediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+            @Override
+            public void onBufferingUpdate(MediaPlayer mp, int percent) {
+                binding.seekbarTime.setSecondaryProgress(percent);
+            }
+        });
+    }
+    private void prepareMediaPlayer(){
+        String path = getIntent().getStringExtra("data");
+        try {
+            mediaPlayer.setDataSource(path);
+            mediaPlayer.prepare();
+            binding.end.setText(milliSecondsToTimer(mediaPlayer.getDuration()));
+            mediaPlayer.start();
+        }
+        catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void restart(){
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mediaPlayer.reset();
+            }
+        });
+    }
+    private Runnable updater = new Runnable() {
+        @Override
+        public void run() {
+            updateSeekbar();
+            long currentDuration = mediaPlayer.getCurrentPosition();
+            binding.start.setText(milliSecondsToTimer(currentDuration));
+            long updateDuration = (mediaPlayer.getDuration() - mediaPlayer.getCurrentPosition());
+            binding.end.setText(milliSecondsToTimer(updateDuration));
+        }
+    };
+
+    private void updateSeekbar(){
+        if (mediaPlayer.isPlaying()){
+            binding.seekbarTime.setProgress((int) (((float) mediaPlayer.getCurrentPosition() / mediaPlayer.getDuration()) * 100));
+            handler.postDelayed(updater, 1000);
+        }
     }
 
-    private void SongNames() {
-        if (currentIndex == 0) {
-            songTile.setText("Chung ta khong thuoc ve nhau - Son Tung MTP");
-            imageView.setImageResource((R.drawable.chungtakothuocvenhau));
+    private String milliSecondsToTimer(long milliseconds){
+        String timerString = "";
+        String seconsString;
+
+        int hours = (int)(milliseconds / (1000*60*60));
+        int minutes = (int) (milliseconds % (1000*60*60))/(1000*60);
+        int seconds = (int) ((milliseconds % (1000*60*60))%(1000*60)/1000);
+
+        if (hours > 0){
+            timerString = hours+":";
         }
-        if (currentIndex == 1) {
-            songTile.setText("Co chac yeu la day - Son Tung MTP");
-            imageView.setImageResource((R.drawable.chungtakothuocvenhau));
+        if (seconds<10){
+            seconsString = "0" +seconds;
+        }else
+        {
+            seconsString = "" + seconds;
         }
-        if (currentIndex == 2) {
-            songTile.setText("Fashion tan gai - LowG");
-            imageView.setImageResource((R.drawable.chungtakothuocvenhau));
-        }
-        if (currentIndex == 3) {
-            songTile.setText("Hay trao cho anh - Son Tung MTP");
-            imageView.setImageResource((R.drawable.chungtakothuocvenhau));
-        }
-        if (currentIndex == 4) {
-            songTile.setText("Luon yeu doi - LowG");
-            imageView.setImageResource((R.drawable.chungtakothuocvenhau));
-        }
-        if (currentIndex == 5) {
-            songTile.setText("Noi nay co anh - Son Tung MTP");
-            imageView.setImageResource((R.drawable.chungtakothuocvenhau));
+        timerString = timerString + minutes +":"+ seconsString;
+        return timerString;
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Giải phóng nguồn tài nguyên khi không sử dụng nữa
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
         }
     }
+
 }
--- app/src/main/java/com/landz/android/database/Database.java
+++ app/src/main/java/com/landz/android/database/Database.java
@@ -1,18 +1,29 @@
 package com.landz.android.database;
 
+import static android.provider.MediaStore.Audio.AudioColumns.ALBUM_ID;
+import static android.provider.MediaStore.Audio.AudioColumns.ARTIST_ID;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
-import android.graphics.Bitmap;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import com.landz.android.activity.List_song_in_album;
+import com.landz.android.activity.play_music;
+
+import java.util.ArrayList;
+
+import Model.Artist;
+import Model.Song;
+import Model.album;
 
 
 public class Database extends SQLiteOpenHelper {
@@ -35,8 +46,6 @@ public class Database extends SQLiteOpenHelper {
         super.onOpen(db);
         db.execSQL("PRAGMA cursor_window = 5000000");
     }
-
-
     public Boolean insertUser(String username, String password) {
         SQLiteDatabase db = this.getWritableDatabase();
 
@@ -76,5 +85,151 @@ public class Database extends SQLiteOpenHelper {
             return false;
         }
     }
+
+
+    public ArrayList<album> getAllAlbum() {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT album.*, artist.artist_Name FROM album INNER JOIN artist ON album.artist_Id = artist.artist_Id";
+        Cursor cursor = null;
+        if (db != null) {
+            cursor = db.rawQuery(query, null);
+        }
+        if (cursor == null) {
+            return null;
+        } else {
+            ArrayList<album> albums = new ArrayList<>();
+            while (cursor.moveToNext()) {
+                int id = cursor.getInt(0);
+                String albumName = cursor.getString(2);
+                String release = cursor.getString(3);
+                byte[] imageContent = cursor.getBlob(4);
+                String artistName = cursor.getString(5);
+
+                album s = new album( albumName, release,artistName);
+                s.setThumb(imageContent);
+                s.setAlbum_id(id);
+                albums.add(s);
+            }
+            return albums;
+        }
+    }
+    public ArrayList<Song> getAllSong(){
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT song.*, artist.artist_Name FROM song INNER JOIN artist ON song.artist_Id = artist.artist_Id";
+        Cursor cursor = null;
+        if(db != null){
+            cursor = db.rawQuery(query, null);
+        }
+        if (cursor == null){
+            return null;
+        }
+        else {
+            ArrayList<Song> songs = new ArrayList<>();
+            while (cursor.moveToNext()){
+                int Song_id = cursor.getInt(0);
+                String title = cursor.getString(3);
+                String duration = cursor.getString(4);
+                byte[] imageContent = cursor.getBlob(5);
+                String path = cursor.getString(6);
+                String art = cursor.getString(7);
+                Song s = new Song(title,art,duration,path);
+                s.setSong_Image(imageContent);
+                s.setSong_Id(Song_id);
+                songs.add(s);
+            }
+            return songs;
+        }
+    }
+
+    public ArrayList<Song> getSongsForAlbum(int albumId) {
+        ArrayList<Song> songList = new ArrayList<>();
+        String selectQuery = "SELECT song.*, artist.artist_Name FROM song INNER JOIN artist ON song.artist_Id = artist.artist_Id " + " WHERE " + "album_Id" + " = ?";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, new String[]{String.valueOf(albumId)});
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+
+                    int Song_id = cursor.getInt(0);
+                    String title = cursor.getString(3);
+                    String duration = cursor.getString(4);
+                    byte[] imageContent = cursor.getBlob(5);
+                    String path = cursor.getString(6);
+                    String art = cursor.getString(7);
+                    Song s = new Song(title,art,duration,path);
+                    s.setSong_Image(imageContent);
+                    s.setSong_Id(Song_id);
+                    songList.add(s);
+                } while (cursor.moveToNext());
+            }
+        }
+        finally {
+            cursor.close();
+            db.close();
+        }
+
+        return songList;
+    }
+
+    public Song getsongbyId(int songid){
+        String selectQuery = "SELECT * FROM song "+" WHERE " + "song_Id" + " = ?";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, new String[]{String.valueOf(songid)});
+        Song s = new Song();
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+
+                    int Song_id = cursor.getInt(0);
+                    String title = cursor.getString(3);
+                    byte[] imageContent = cursor.getBlob(5);
+                    String path = cursor.getString(6);
+                    s.setSong_Title(title);
+                    s.setSong_Image(imageContent);
+                    s.setPath(path);
+                    s.setSong_Id(Song_id);
+
+                } while (cursor.moveToNext());
+            }
+
+        }
+        finally {
+            cursor.close();
+            db.close();
+        }
+        return s;
+
+    }
+//    public ArrayList<Song> getAllSongbyID(int songid) {
+//        ArrayList<Song> songList = new ArrayList<>();
+//        String selectQuery = "SELECT song.*, artist.artist_Name FROM song INNER JOIN artist ON song.artist_Id = artist.artist_Id " + " WHERE " + "song_Id" + " = ?";
+//        SQLiteDatabase db = this.getReadableDatabase();
+//        Cursor cursor = db.rawQuery(selectQuery, new String[]{String.valueOf(songid)});
+//        try {
+//            if (cursor.moveToFirst()) {
+//                do {
+//
+//                    int Song_id = cursor.getInt(0);
+//                    String title = cursor.getString(3);
+//                    String duration = cursor.getString(4);
+//                    byte[] imageContent = cursor.getBlob(5);
+//                    String path = cursor.getString(6);
+//                    String art = cursor.getString(7);
+//                    Song s = new Song(title,art,duration,path);
+//                    s.setSong_Image(imageContent);
+//                    s.setSong_Id(Song_id);
+//                    songList.add(s);
+//                } while (cursor.moveToNext());
+//            }
+//        }
+//        finally {
+//            cursor.close();
+//            db.close();
+//        }
+//
+//        return songList;
+//    }
 
 }
