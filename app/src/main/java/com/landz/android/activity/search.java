376e5cbd0496180174542d68c719bc6f709c4f32
hoang <hoangngong1920@gmail.com>
2023-12-19 14:19:32 +0700

    search

--- app/src/main/java/com/landz/android/activity/search.java
+++ app/src/main/java/com/landz/android/activity/search.java
@@ -2,27 +2,30 @@ package com.landz.android.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.landz.android.R;
+import com.landz.android.database.Database;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import Model.LibraryAdapter;
-import Model.library;
+import Model.Song;
+import Model.SongAdapter;
+import Model.itemSearch;
+import Model.itemSearchAdapter;
 
 public class search extends AppCompatActivity {
-    private SearchView searchView;
-    private RecyclerView recyclerView;
-    private List<library> mLibraries;
-    private LibraryAdapter libraryAdapter;
 
+
+    SearchView searchView;
+    ArrayList<Song> songArrayList;
+    SongAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,37 +41,37 @@ public class search extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                filterList(newText);
+                searchList(newText);
                 return true;
             }
         });
-
-        recyclerView = findViewById(R.id.listSearchItem);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mLibraries = new ArrayList<>();
-
-        mLibraries.add(new library(R.drawable.img_heart, "Bài hát ưa thích", "ds,"));
-        mLibraries.add(new library(R.drawable.img_artist_1, "Sơn Tùng M-TP", "bf"));
-        mLibraries.add(new library(R.drawable.img_artist_2, "Low G", "sdj"));
-        mLibraries.add(new library(R.drawable.img_artist_3, "Taylor Swift", "jfdh"));
-        mLibraries.add(new library(R.drawable.img_heart, "Bài hát ưa thích", "sd"));
-        mLibraries.add(new library(R.drawable.img_heart, "Bài hát ưa thích", "mdmg"));
+        Database db = new Database(this);
+        songArrayList = db.getAllSong();
+        RecyclerView rcv = findViewById(R.id.rcvSearchItem);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        rcv.addItemDecoration(itemDecoration);
+        adapter = new SongAdapter(this, songArrayList);
+        rcv.setAdapter(adapter);
+        rcv.hasFixedSize();
+        rcv.setLayoutManager(new LinearLayoutManager(this));
 
 
     }
 
-    private void filterList(String text) {
-        List<library> fillteredList = new ArrayList<>();
-        for (library library : mLibraries) {
-            if (library.getNameLib().toLowerCase().contains(text.toLowerCase())) {
-                fillteredList.add(library);
+    private void searchList(String Text) {
+        ArrayList<Song> songsSearchList = new ArrayList<>();
+        for (Song song : songArrayList){
+            if (song.getSong_Title().toLowerCase().contains(Text.toLowerCase())) {
+                songsSearchList.add(song);
             }
         }
-        if (fillteredList.isEmpty()) {
-            Toast.makeText(this, "No data found", Toast.LENGTH_SHORT).show();
-        } else {
-
+        if (songsSearchList.isEmpty()){
+            Toast.makeText(this,"Not found", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            adapter.setSearchList(songsSearchList);
         }
     }
+
+
 }
